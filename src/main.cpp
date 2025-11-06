963c683ea007e8a85431b99c3817f90243b6fa8f
Bcalero558 <Bcalero234@gmail.com>
2025-11-05 22:12:20 -0500

    Implement_window_open

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,62 @@
-
-
+// platform global variables
 static bool running = true;
 
+// platform functions
+bool platform_create_window(char* title, int width, int height);
+
+//windows platform implementation
+// used if the target platform is windows by checking if windows 32 bit is the current operating system
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+// windows global variables- adds variables for windows platform 
+
+//windows platform implementations- makes the global function specific to windows platform
+bool platform_create_window(char* title, int width, int height) 
+{
+    // gets the instance of the current module in order to process what is happening in the window
+    HINSTANCE instance = GetModuleHandleA(0);
+
+    WNDCLASSA wc = {};
+    // sets the window class properties
+    wc.hInstance = instance;
+    // the icon for the window and the cursor style
+    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(0, IDC_ARROW);
+    wc.lpszClassName = title;// identifier of window not the title
+    
+    wc.lpfnWndProc = DefWindowProcA; // long funtion pointer to the window proc address // responsible for processing messages sent to the window
+
+    //ALLOW FOR REGISTRATION OF THE WINDOW CLASS AND WINDOW CREATION
+    if(!RegisterClassA(&wc)) {
+        return false;
+    }
+    // WINDOWS CREATION DOCCUMENTATION - https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
+
+    HWND window = CreateWindowExA(
+        0,
+        title,
+        title,
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        width, height,
+        0,
+        0,
+        instance,
+        0
+    );
+
+}
+
+
+#endif
+
+
+
 int main () {
+    platform_create_window("Platformer Concept Demo", 1920, 1080);
     while (running)
     {
         // game loop code
